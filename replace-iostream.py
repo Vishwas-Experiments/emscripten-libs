39296b4ff50c19280df9a4b11567f394024d859b
Vishwas-Adiga <vishwasadiga@gmail.com>
2020-10-24 15:35:55 +0530

    feat: Ensure emscripten builds with asm streams

--- replace-iostream.py
+++ replace-iostream.py
@@ -5,8 +5,13 @@ files = glob.glob(os.getcwd() + '/**/*.cpp', recursive=True)
 for file in files:
     r = open(file, 'r')
     contents = r.read()
+    if file.endswith('.h'):
+        continue
     contents = contents.replace('<iostream>', '"asm-lib/asmstream.h"')
     contents = contents.replace('<iostream.h>', '"asm-lib/asmstream.h"')
+    contents = contents.replace('cout', 'asmcout');
+    contents = contents.replace('cin', 'asmcin');
+    contents = contents.replace('cerr', 'asmcerr');
     r.close()
 
     w = open(file, 'w')
